08c3c9821b009d7d6d37fb1ee8fc38fe52dc8c15
Prince Alexander John <princeajohn2005@gmail.com>
2025-04-05 06:09:29 +0400

    Added model, tracker and vid capture

--- num.py
+++ num.py
@@ -4,3 +4,14 @@ from ultralytics import YOLO
 from deep_sort_realtime.deepsort_tracker import DeepSort
 #Importing OpenCV for video capturing
 import cv2
+
+#Pretrained model that can detect trucks
+model = YOLO("yolov8n.pt")
+#Initializing DeepSORT tracker with few parameters
+tracker = DeepSort(max_age=30, n_init=3, nms_max_overlap=1.0)
+#max_age refers to amount of frames required for a tracked object to go undetected before DeepSORT forgets it
+#n_init refers to the amount of frames required to be detected until an object is tracked and assigned an ID
+#nms_max_overlap determines how much overlap between bounding boxes are allowed before they're considered dupes
+
+#Video capture ovject created to read from recording
+capture = cv2.VideoCapture("video.mp4")
