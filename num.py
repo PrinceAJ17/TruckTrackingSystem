2f0588f3506c3a8f3b52543059b0ff59d4d2d0de
Prince Alexander John <princeajohn2005@gmail.com>
2025-04-05 05:54:08 +0400

    Added libraries

--- num.py
+++ num.py
@@ -1,1 +1,6 @@
-print("Testing")
+#Importing object detection model
+from ultralytics import YOLO
+#Importing multi-object tracking class
+from deep_sort_realtime.deepsort_tracker import DeepSort
+#Importing OpenCV for video capturing
+import cv2
